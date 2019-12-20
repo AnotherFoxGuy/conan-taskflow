3c8ecf401b45a9dde08a93a09ddaf600f1871f85
Edgar <Edgar@AnotherFoxGuy.com>
2019-12-20 15:48:12 +0100

    :bug: Fixed missing include dir

--- conanfile.py
+++ conanfile.py
@@ -27,5 +27,4 @@ class taskflowConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.includedirs = 'include'
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include', 'include/taskflow']
